9fb252cc65ee520696199fefbfa308885e6f4aa4
AlexanderMann <mannimal09@gmail.com>
2016-04-16 00:11:19 -0400

    init

---
+++ src/ac/core.clj
@@ -0,0 +1,6 @@
+(ns ac.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/ac/core_test.clj
@@ -0,0 +1,7 @@
+(ns ac.core-test
+  (:require [clojure.test :refer :all]
+            [ac.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
