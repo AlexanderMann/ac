21a6dc5213d076c6f5c8a6b8417394e1aa92364a
AlexanderMann <mannimal09@gmail.com>
2016-04-21 07:24:45 -0400

    adding developer info

--- project.clj
+++ project.clj
@@ -3,4 +3,8 @@
   :url "https://github.com/AlexanderMann/ac"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :pom-addition [:developers [:developer
+                              [:name "Alexander Mann"]
+                              [:url "http://www.alxmnn.com/"]
+                              [:timezone "-4"]]])
